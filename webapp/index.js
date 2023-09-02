4fc3c7809769da03866ba04a098d00a4935e7ec9
samick.hsu <samick.hsu@gmail.com>
2023-09-02 20:56:22 +0800

    1. Updated html view

--- webapp/index.js
+++ webapp/index.js
@@ -33,9 +33,75 @@ function fileExtToMimeType(ext) {
     break;
   }
 }
+function tableHeader(name) {
+  const th = document.createElement('th');
+  th.innerText = name;
+  return th;
+}
+function createTableView(id) {
+  const table = document.createElement('table');
+  table.id = id;
+  const tr = document.createElement('tr');
+  table.append(tr);
+  const th1 = tableHeader('Origin');
+  const th2 = tableHeader('Resized');
+  const th3 = tableHeader('Information');
+  const th4 = tableHeader('Operations');
+  tr.append(th1);
+  tr.append(th2);
+  tr.append(th3);
+  tr.append(th4);
+  document.body.append(table);
+}
+function appendRow(elements) {
+  const table = document.querySelector('table#images');
+  const tr = document.createElement('tr');
+  elements.forEach(element => {
+    const td = document.createElement('td');
+    td.append(element);
+    tr.append(td);
+  });
+  table.append(tr);
+}
+async function loadImage(url) {
+  return new Promise(resolve => {
+    const image = document.createElement('img');
+    image.addEventListener('load', () => {
+      resolve(image);
+    });
+    image.src = url;
+  });
+}
+async function displayImage(originFile, resizedFile) {
+  const url1 = URL.createObjectURL(originFile);
+  const image1 = await loadImage(url1);
+
+  const url2 = URL.createObjectURL(resizedFile);
+  const image2 = await loadImage(url2);
+
+  const information = document.createElement('div');
+  [
+  `Origin File Size: ${originFile.size}`,
+  `Origin Image Size: ${image1.width} x ${image1.height}`,
+  `Resized File Size: ${resizedFile.size}`,
+  `Resized Image Size: ${image2.width} x ${image2.height}`,
+  ].forEach(text => {
+    const element = document.createElement('div');;
+    element.innerText = text;
+    information.append(element);
+  });
+
+  const btnDownload = document.createElement('button');
+  btnDownload.innerText = 'Download';
+  btnDownload.addEventListener('click', () => {
+    saveFile(file.name, file, true);
+  });
+  appendRow([image1, image2, information, btnDownload]);
+}
 (() => {
   async function main() {
     await init();
+    createTableView('images');
     async function start() {
       const file = await openFile(['.png', '.jpg']);
       const fileExt = file.name.substring(file.name.lastIndexOf('.'));
@@ -49,8 +115,8 @@ function fileExtToMimeType(ext) {
           const option = {
             type: fileExtToMimeType(fileExt),
           };
-          const file = new File([data.buffer], 'resized' + fileExt, option);
-          saveFile(file.name, file, true);
+          const resizedFile = new File([data.buffer], 'resized' + fileExt, option);
+          displayImage(file, resizedFile);
         }
       });
       resize_image(fileExt, 300, 300);
